63057c5d254c3028345ab79d1b6275484f7093fa
Victor Ng <vng@brandeis.edu>
2020-08-02 12:00:03 -0500

    bot update

--- bot.py
+++ bot.py
@@ -141,4 +141,15 @@ async def animerec(ctx):
 
     await ctx.send(embed=embed)
 
+
+@bot.command(brief='Rolls a 20 sided dice', description='Rolls a twenty sided dice')
+async def diceroll(ctx):
+
+    dice_roll = randint(1, 20)
+
+    embed = discord.Embed()
+    embed.add_field(name="Roll", value=dice_roll)
+
+    await ctx.send(embed=embed)
+
 bot.run(TOKEN)
--- whitehackcommands.py
+++
@@ -1,9 +0,0 @@
-@bot.command(brief='Rolls a 20 sided dice', description='Rolls a twenty sided dice')
-async def diceroll(ctx):
-
-    dice_roll = randint(1, 20)
-
-    embed = discord.Embed()
-    embed.add_field(name="Roll", value=dice_roll)
-
-    await ctx.send(embed=embed)
